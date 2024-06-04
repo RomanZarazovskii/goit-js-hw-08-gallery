5f62fdb3a6b6285ef451aef6562e041666cdf916
RomanZarazovskii <124078828+RomanZarazovskii@users.noreply.github.com>
2024-06-04 16:33:35 +0300

    update

---
+++ index.js
@@ -0,0 +1,127 @@
+const galleryItems = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+const galleryContainer = document.querySelector('.js-gallery');
+
+const createGallery = ({ preview, original, description }) => `
+  <li class="gallery__item">
+    <a href="${original}" class="gallery__link">
+      <img src="${preview}" alt="${description}" class="gallery__image" data-source="${original}" />
+    </a>
+  </li>
+`;
+
+const gallery = galleryItems.map(createGallery).join('');
+
+galleryContainer.innerHTML = gallery;
+
+const lightbox = document.querySelector('.js-lightbox');
+const lightboxOverlay = document.querySelector('.lightbox__overlay');
+const lightboxImage = document.querySelector('.lightbox__image');
+const closeButton = document.querySelector('.lightbox__button');
+
+galleryContainer.addEventListener('click', onContainerClick);
+
+function onContainerClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') return;
+  openModal(e.target.dataset.source, e.target.alt);
+}
+
+function openModal(src, alt) {
+  lightbox.classList.add('is-open');
+  lightboxImage.src = src;
+  lightboxImage.alt = alt;
+}
+
+closeButton.addEventListener('click', closeModal);
+lightboxOverlay.addEventListener('click', closeModal);
+
+function closeModal() {
+  lightbox.classList.remove('is-open');
+  lightboxImage.src = '';
+  lightboxImage.alt = '';
+}
+
+window.addEventListener('keydown', onKeyPress);
+
+function onKeyPress(e) {
+  if (e.code === 'Escape') {
+    closeModal();
+  }
+  if (e.code === 'ArrowRight' || e.code === 'ArrowLeft') {
+    const currentIndex = galleryItems.findIndex(item => item.original === lightboxImage.src);
+    let newIndex;
+
+    if (e.code === 'ArrowRight') {
+      newIndex = (currentIndex + 1) % galleryItems.length;
+    } else {
+      newIndex = (currentIndex - 1 + galleryItems.length) % galleryItems.length;
+    }
+
+    openModal(galleryItems[newIndex].original, galleryItems[newIndex].description);
+  }
+};
